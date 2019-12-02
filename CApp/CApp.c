c9629cf86ca5ae91da2177852dc163ac3226024f
Frederik <frederik@zenziot.dk>
2019-12-02 12:37:20 +0100

    Update CApp.c

--- CApp/CApp.c
+++ CApp/CApp.c
@@ -2,6 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum MenuOptions
+{
+	PRICE = 0,
+	DEVICES,
+	EXIT,
+	TOTAL, //Must always be the last!
+};
+
 void main(void) {
-	Devices1();
+	int selectionIdx = -1;
+	/*char menuName[TOTAL];
+	menuName[EXIT] = "Exit program";
+	menuName[PRICE] = "Price";
+	menuName[DEVICES] = "Devices";*/
+
+
+
+	printf("Welcome to Energy advisor 1.0");
+
+	switch (selectionIdx) {
+	case PRICE:
+	{
+		break;
+	}
+	case DEVICES:
+	{
+		break;
+	}
+	default:
+	{
+		break;
+	}
+	}
+
 }
