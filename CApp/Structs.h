82e0cfc20b83943d62367f1fab8eb2b02fad359e
Micklas <myselfluls@ioexception.at>
2019-12-06 12:58:28 +0100

    Properly formatted struct file

--- CApp/Structs.h
+++ CApp/Structs.h
@@ -2,8 +2,16 @@
 
 #define MAX_DEVICE_NAME 50
 
-typedef struct devices {
+typedef struct _Devices {
 	char deviceName[MAX_DEVICE_NAME];
 	int kwh;
 } devices;
 
+/* Used to parse array from file into */
+typedef struct _DateTimePrice {
+	int day;
+	int month;
+	int year;
+	int hourStart;
+	int hourEnd;
+} dateTimePrice;
--- CApp/getHourPrices.c
+++ CApp/getHourPrices.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#include "Structs.h"
 
 
 int getHourPrice(void) {
 	char array[] = { "01-01-2019;00-01;162,28" };
-
+	
 }
 
