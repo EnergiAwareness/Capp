fc4a07a8baca9733e8a8df2a084a69a87b4f7bf2
Micklas <MySelfLuls@users.noreply.github.com>
2019-12-10 16:32:12 +0100

    (Probably) fixed PriceData.c
    However it causes exception error in Prices.c

--- CApp/PriceData.c
+++ CApp/PriceData.c
@@ -7,30 +7,34 @@
 
 
 int GetHourPrice(int startDay, int startMonth, int endDay, int endMonth, _DateTimePrice* dateTimePrice, int* sizeOfStruct) {
-	int fileHeight = 0, i = 0, errorCode = UNKNOWN_ERROR;
+	int count = 0, fileHeight = 0, i = 0, x = 0, errorCode = UNKNOWN_ERROR;
 	char** loadedFileArray = NULL;
 	char* temp;
-	int count = 0;
 	char delim[] = ";-";
-	int day, month;
+	double data[6];
 	
 	if ((errorCode = LoadFile("elspot-prices_2018_hourly_dkk.csv", &loadedFileArray, &fileHeight)) == OK) {
 		_DateTimePrice* builder = calloc(sizeof(_DateTimePrice), fileHeight);
 		
 		for (i = 0; i < fileHeight; i++) {
 			temp = strtok(loadedFileArray[i], delim);
-			day = getc(temp[0]);
-			month = getc(temp[1]);
-			if ((startDay >= day && startMonth >= month) || (endDay <= day && endMonth <= month)) {
-				builder[i].day = atoi(temp[0]);
-				builder[i].month = atoi(temp[1]);
-				builder[i].year = atoi(temp[2]);
-				builder[i].hourStart = atoi(temp[3]);
-				builder[i].hourEnd = atoi(temp[4]);
-				builder[i].price = atof(temp[5]);
+			for (x = 0; x < 6; x++) {
+				if (temp != NULL) {
+					data[x] = atof(temp);
+				}
+				else { break; }
+				temp = strtok(NULL, delim);
+			}
+
+			if ((startDay >= data[0] && startMonth >= data[1]) || (endDay <= data[0] && endMonth <= data[1])) {
+				builder[i].day = data[0];
+				builder[i].month = data[1];
+				builder[i].year = data[2];
+				builder[i].hourStart = data[3];
+				builder[i].hourEnd = data[4];
+				builder[i].price = data[5];
 				count++;
 			}
-			temp = strtok(NULL, delim);
 		}
 	}
 
