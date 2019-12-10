1acff23b683a2918d5addfe079c22605004fb96e
BananDK <58289156+BananDK@users.noreply.github.com>
2019-12-10 11:06:01 +0100

    Update Prices.c

--- CApp/Prices.c
+++ CApp/Prices.c
@@ -13,10 +13,8 @@ int Today() {
 	struct tm tm = *localtime(&t);
 	_DateTimePrice* callPrices = NULL;
 	size_t structSize = 0;
-	int i = 0;
 	int errorCode = UNKNOWN_ERROR;
-	if ((errorCode = ParseFileToStruct(tm.tm_mday, tm.tm_mon + 1, tm.tm_mday, tm.tm_mon + 1, callPrices, &structSize)) == OK)
-	{
+	if ((errorCode = ParseFileToStruct(tm.tm_mday, tm.tm_mon + 1, tm.tm_mday, tm.tm_mon + 1, callPrices, &structSize)) == OK) {
 		PrintOutPriceData(callPrices, structSize);
 	}
 
@@ -31,8 +29,7 @@ int Tomorrow() {
 	int i = 0;
 	int errorCode = UNKNOWN_ERROR;
 
-	if ((errorCode = ParseFileToStruct(tm.tm_mday + 1, tm.tm_mon + 1, tm.tm_mday + 1, tm.tm_mon + 1, callPrices, &structSize)) == OK)
-	{
+	if ((errorCode = ParseFileToStruct(tm.tm_mday + 1, tm.tm_mon + 1, tm.tm_mday + 1, tm.tm_mon + 1, callPrices, &structSize)) == OK) {
 		PrintOutPriceData(callPrices, structSize);
 	}
 
@@ -44,14 +41,12 @@ int Historical() {
 	struct tm tm = *localtime(&t);
 	_DateTimePrice* callPrices = NULL;
 	size_t structSize = 0;
-	int i = 0;
 	int errorCode = UNKNOWN_ERROR;
 
-	GetTextString(13);
+	printf("GetTextString\n", DATE_INPUT);
 	scanf_s("%d:%d %d:%d", &tm.tm_mday, &tm.tm_mon + 1, &tm.tm_mday, &tm.tm_mon + 1);
 
-	if ((errorCode = ParseFileToStruct(tm.tm_mday, tm.tm_mon + 1, tm.tm_mday, tm.tm_mon + 1, callPrices, &structSize)) == OK)
-	{
+	if ((errorCode = ParseFileToStruct(tm.tm_mday, tm.tm_mon + 1, tm.tm_mday, tm.tm_mon + 1, callPrices, &structSize)) == OK) {
 		PrintOutPriceData(callPrices, structSize);
 	}
 
@@ -62,6 +57,6 @@ void PrintOutPriceData(_DateTimePrice* prices, int cnt)
 {
 	int i;
 	for (i = 0; i < cnt; i++) {
-		printf("%d - %d %d-%d: %2.lf dkk", prices->day, prices->month, prices->hourStart, prices->hourEnd, prices->price);
+		printf("%d - %d %d-%d: %2.lf dkk", prices[i].day, prices[i].month, prices[i].hourStart, prices[i].hourEnd, prices[i].price);
 	}
 }
